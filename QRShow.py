6fed8d6dd6c63dec4ec50e82ad1945d2cabef443
hssnirkh <hassan.irankhah@gmail.com>
2022-09-05 22:28:55 +0430

    udate 1

--- QRShow.py
+++ QRShow.py
@@ -3,8 +3,8 @@ from pyqrcode import QRCode
 
 Dest = "https://github.com/hssnirkh"
 
-MyQR = QRCode(Dest)
+MyQR = QRCode(Dest,error='H',version=None,mode=None,encoding='iso-8859-1')
 
-MyQR.png('qrcode2.png',scale=8)
+MyQR.png('qrcode3.png',scale=8)
 
 MyQR.show()
